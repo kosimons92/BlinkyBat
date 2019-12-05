a12e85828552d3e57ff29084a85f5579a1dfcb66
kosimons92 <54904282+kosimons92@users.noreply.github.com>
2019-12-05 18:17:21 -0500

    Add files via upload

---
+++ Assets/ToDo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToDo : MonoBehaviour
+{
+/*
+* Add Ceiling
+* Animate Ceiling and Floor
+* Add lighting & effects
+* Echolocate function
+* UI
+*/
+}
